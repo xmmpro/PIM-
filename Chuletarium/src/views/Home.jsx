c6aacff08a0e4a682f0cf14b8dfe1113af588709
ximo <ximomonfort@gmail.com>
2025-12-11 22:05:48 +0100

    Añadiendo archivos reales de Chuletarium al repositorio

---
+++ Chuletarium/src/views/Home.jsx
@@ -0,0 +1,9 @@
+// src/views/Home.jsx
+export default function Home() {
+  return (
+    <main>
+      <h1>Chuletarium</h1>
+      <p>Bienvenido a tu web de chuletas de programación 😎</p>
+    </main>
+  );
+}
